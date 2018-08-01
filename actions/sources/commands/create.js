24c94794e0a816eb430fd4ba81e5d2ff1b0b11ab
Layne Faler <laynefaler@gmail.com>
2018-08-01 12:17:53 -0700

    Updates to commands for CLI create

--- actions/sources/commands/create.js
+++ actions/sources/commands/create.js
@@ -8,29 +8,27 @@ const description = 'Generate a CLI Build folder into your current directory';
 
 const command = (directoryName) => {
     const root = process.cwd();
-    ncp(path.join(__dirname, '..', '..', '..', 'templates', 'cli'), path.join(root, directoryName), () => {
-        prompt.start();
-        prompt.get({
-            properties: {
-                'name': { required: true },
-                'title': { required: true },
-                'commandName': { required: true },
-            },
-        }, function (err, result) {
-            console.log(result);
-            if (err) {
-                console.error(`Error with your CLI creation.`);
-            } else {
-                const object = {};
-                const pack = require('../../../package.json');
-                pack.name = result.name;
-                pack.title = result.title;
-                object[result.commandName] = './actions/index.js';
-                pack.bin = object;
-                fs.writeFileSync(path.join(root, 'package.json'), JSON.stringify(pack, null, 4));
-                console.log(`Your CLI directory has been built in "${root}/${directoryName}"`);
-            }
-        });
+    ncp(path.join(__dirname, '..', '..', '..', 'templates', 'cli'), path.join(root, directoryName));
+    prompt.start();
+    prompt.get({
+        properties: {
+            'name': { required: true },
+            'title': { required: true },
+            'commandName': { required: true },
+        },
+    }, function (err, result) {
+        if (err) {
+            console.error(`Error with your CLI creation.`);
+        } else {
+            const object = {};
+            let pack = require('../../../package.json');
+            pack.name = result.name;
+            pack.title = result.title;
+            object[result.commandName] = './actions/index.js';
+            pack.bin = object;
+            fs.writeFileSync(path.join(root, 'package.json'), JSON.stringify(pack, null, 4));
+            console.log(`Your CLI directory has been built in "${root}/${directoryName}"`);
+        }
     });
 };
 
